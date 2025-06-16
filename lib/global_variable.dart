157c3c41ea28e574533cfa6051fb53df8cde1928
Bhavya Kothari <bhavyaskothari@gmail.com>
2025-06-16 19:51:38 +0530

    Add files via upload

---
+++ lib/global_variable.dart
@@ -0,0 +1,79 @@
+final products = [
+  {
+    'id' : '0',
+    'title': 'Nike Air Jordan Travis',
+    'price': 129.99,
+    'size': [7,8,9,10,11],
+    'company': 'Nike',
+    'imageUrl':'assets/images/shoe_nike_5.png'
+  },
+
+  {
+    'id' : '1',
+    'title': 'Puma Radiant Lows',
+    'price': 99.99,
+    'size': [7,8,9,10],
+    'company': 'Puma',
+    'imageUrl':'assets/images/shoe_puma_2.png'
+  },
+
+  {
+    'id' : '2',
+    'title': 'Nike Air Max',
+    'price': 59.99,
+    'size': [7,8,11],
+    'company': 'Nike',
+    'imageUrl':'assets/images/shoe_nike_6.png'
+  },
+
+  {
+    'id' : '3',
+    'title': 'Asics Gel Kayano 9',
+    'price': 159.99,
+    'size': [9,10,11],
+    'company': 'Asics',
+    'imageUrl':'assets/images/shoe_asics_2.png'
+  },
+
+  {
+    'id' : '4',
+    'title': 'Adidas Ultraboost',
+    'price': 129.99,
+    'size': [7,8,9,10,11],
+    'company': 'Adidas',
+    'imageUrl':'assets/images/shoe_adidas_3.png'
+  },
+
+  {
+    'id' : '5',
+    'title': 'Nike UltraMax 90',
+    'price': 89.99,
+    'size': [7,8,10,11],
+    'company': 'Nike',
+    'imageUrl':'assets/images/shoe_nike_7.png'
+  },
+
+  {
+    'id' : '6',
+    'title': 'Adidas Superstar 21',
+    'price': 109.99,
+    'size': [7,9,11],
+    'company': 'Adidas',
+    'imageUrl':'assets/images/shoe_adidas_4.png'
+  },
+
+
+];
+
+
+final cart = [
+  {
+    'id' : '0',
+    'title': 'Nike Air Jordan Travis',
+    'price': 129.99,
+    'size': 9,
+    'company': 'Nike',
+    'imageUrl':'assets/images/shoe_nike_3.jpeg',
+    'quantity': 1
+  }
+];
